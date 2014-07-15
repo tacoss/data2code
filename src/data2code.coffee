2f7fc01ec2d07baf3e3c39a14d16fbe6e79decd5
atomsfat <atomsmail@gmail.com>
2014-07-15 08:10:15 -0500

    100 % cobertura

--- src/data2code.coffee
+++ src/data2code.coffee
@@ -5,7 +5,7 @@ module.exports.process = (data , gen) ->
   # console.log "Data2Code#process", gen
   # console.log "Data2Code#process.data", data
   if gen.handleRender and typeof gen.handleRender is 'function'
-    template = Handlebars.compile(gen.template)
+
     if gen.helpers and gen.helpers instanceof Array
       for helper in gen.helpers
         if helper.name and helper.fn and typeof helper.fn is 'function'
@@ -15,11 +15,20 @@ module.exports.process = (data , gen) ->
         if partial.name and partial.str
           Handlebars.registerPartial(partial.name, partial.str)
 
+
     if typeof gen.parser is 'function'
       dataParsed = gen.parser(data)
     else
       dataParsed  = data
 
-    gen.handleRender(template(dataParsed))
+    template = Handlebars.compile(gen.template)
+    try
+      gen.handleRender(template(dataParsed))
+    catch e
+      console.log e
+
+
+
+
 
 
--- test/data2code-spec.js
+++ test/data2code-spec.js
@@ -1,62 +1,81 @@
 'use strict';
 var raml = require('raml-parser'), expect = require('chai').expect;
 var data2Code = new require('../lib/data2code');
-describe('simple code', function () {
-  describe('simple code gen', function () {
 
-    var handleRender = function (done, expectedResult, result) {
-      try {
-        expect(result).to.equal(expectedResult);
-        done();
-      } catch (x) {
-        done(x);
-      }
-    };
+describe('data2code basic test', function () {
 
-    it('should generate something', function (done) {
-      var simpleGen = {};
-      simpleGen.template = 'class {{title}}';
-      simpleGen.handleRender = handleRender.bind(undefined, done, "class Compra venta de gatitos");
+  var handleRender = function (done, expectedResult, result) {
+    try {
+      expect(result).to.equal(expectedResult);
+      done();
+    } catch (x) {
+      done(x);
+    }
+  };
 
-      raml.loadFile('./test/cats.raml').then(function (data) {
-        data2Code.process(data, simpleGen);
-      }, function (error) {
-        console.log('Error parsing: ' + error);
-      });
+  it('should generate something', function (done) {
+    var simpleGen = {};
+    simpleGen.template = '{{title}}';
+    simpleGen.handleRender = handleRender.bind(undefined, done, "Compra venta de gatitos");
 
-    });
-
-    it('testing parser', function (done) {
-      var simpleGen = {};
-      simpleGen.template = 'class {{title}}';
-      simpleGen.parser = function (data) {
-        return {title: data.title + " Hi"}
-      };
-
-      simpleGen.handleRender = handleRender.bind(undefined, done, "class Compra venta de gatitos Hi");
-
-      raml.loadFile('./test/cats.raml').then(function (data) {
-        data2Code.process(data, simpleGen);
-      }, function (error) {
-        console.log('Error parsing: ' + error);
-      });
-
-    });
-
-    it('testing helpers', function (done){
-      var simpleGen = {};
-      simpleGen.template = 'class {{hi title}}';
-      simpleGen.helpers = [{name:'hi', fn:function(msg){ return 'hello ' + msg } }];
-      simpleGen.handleRender = handleRender.bind(undefined, done, "class hello Compra venta de gatitos");
-
-      raml.loadFile('./test/cats.raml').then(function (data) {
-        data2Code.process(data, simpleGen);
-      }, function (error) {
-        console.log('Error parsing: ' + error);
-      });
-
+    raml.loadFile('./test/cats.raml').then(function (data) {
+      data2Code.process(data, simpleGen);
+    }, function (error) {
+      console.log('Error parsing: ' + error);
     });
 
   });
+
+  it('testing parser', function (done) {
+    var simpleGen = {};
+    simpleGen.template = '{{title}}';
+    simpleGen.parser = function (data) {
+      return {title: data.title + " finos"}
+    };
+
+    simpleGen.handleRender = handleRender.bind(undefined, done, "Compra venta de gatitos finos");
+
+    raml.loadFile('./test/cats.raml').then(function (data) {
+      data2Code.process(data, simpleGen);
+    }, function (error) {
+      console.log('Error parsing: ' + error);
+    });
+
+  });
+
+  it('testing helpers', function (done) {
+    var simpleGen = {};
+    simpleGen.template = '{{msg title}}';
+    simpleGen.helpers = [
+      {name: 'msg', fn: function (msg) {
+        return 'Renta, ' + msg
+      } }
+    ];
+    simpleGen.handleRender = handleRender.bind(undefined, done, "Renta, Compra venta de gatitos");
+
+    raml.loadFile('./test/cats.raml').then(function (data) {
+      data2Code.process(data, simpleGen);
+    }, function (error) {
+      console.log('Error parsing: ' + error);
+    });
+
+  });
+
+  it('testing partials', function (done) {
+    var simpleGen = {};
+    simpleGen.template = '{{>header}}';
+    simpleGen.partials = [
+      {name: 'header', str: 'Testing partial'}
+    ];
+    simpleGen.handleRender = handleRender.bind(undefined, done, "Testing partial");
+
+    raml.loadFile('./test/cats.raml').then(function (data) {
+      data2Code.process(data, simpleGen);
+    }, function (error) {
+      console.log('Error parsing: ' + error);
+    });
+
+  });
+
 
 });
